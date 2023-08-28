cec3946ab949467d865779bc6a81a5487136701a
sinagaki <soma0822soma@gmail.com>
2023-08-28 07:12:53 +0000

    add input btn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  route 'static_pages/home'
+  root 'static_pages#home'
   get 'help', to: 'static_pages#help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
