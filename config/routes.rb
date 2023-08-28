1c193d272df778161e8c4f7bb1308d91d4f49d25
sinagaki <soma0822soma@gmail.com>
2023-08-28 06:29:07 +0000

    add model, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  route 'static_pages/home'
+  get 'help', to: 'static_pages#help'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
