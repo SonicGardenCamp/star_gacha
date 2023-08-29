9a69a1de01b39585cfc9ea12e47106728977d5da
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-29 08:21:42 +0000

    ADD:sum_price and sum_cal

--- app/controllers/gacha_controller.rb
+++ app/controllers/gacha_controller.rb
@@ -3,11 +3,15 @@ class GachaController < ApplicationController
     max = params[:max].to_i
     result_last = []
     @result = []
+    @sum_price = 0
+    @sum_cal = 0
     items = Item.where("price <= ?", max)
     while (items.any?)
         @result.push(items.shuffle[0])
       result_last = @result.last
       max -=  result_last["price"]
+      @sum_price += result_last.price
+      @sum_cal += result_last.cal
       items = Item.where("price <= ?", max)
     end
   end
