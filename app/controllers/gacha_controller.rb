381f740fd649c17b18991ac298afaa122c394b74
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-29 01:05:03 +0000

    DEL:gacha/spin

--- app/controllers/gacha_controller.rb
+++ app/controllers/gacha_controller.rb
@@ -1,16 +1,20 @@
 class GachaController < ApplicationController
-  def spin
-    max = params[:hidden_data].to_i
-    items = Item.where("price <= ?", max)
-    while (items.any?)
-      if @result == nil
-        @result = items.shuffle[0]
-      else
-        @result.push(items.shuffle[0])
-      end
-      max -= @result.last["price"].to_i
-      items = Item.where("price <= ?", max)
-    end
-    debugger 
-  end  
+  # def spin
+    # max = params[:hidden_data].to_i
+    # items = Item.where("price <= ?", max)
+    # result_last = []
+    # @result = []
+    # while (items.any?)
+    #   if @result == nil
+    #     @result = items.shuffle[0]
+    #   else
+    #     @result.push(items.shuffle[0])
+    #   end
+    #   result_last = @result.last
+    #   max -=  result_last["price"]
+    #   items = Item.where("price <= ?", max)
+    # end
+    # redirect_to "/help"
+      # redirect_to "/spin"
+  # end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'help', to: 'static_pages#help'
-  post 'gacha/spin', to: 'gacha#spin'
+  post 'spin', to: 'gacha#spin'
+  get 'spin', to: 'gacha#spin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
