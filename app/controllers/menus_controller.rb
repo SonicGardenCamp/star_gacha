51b22f4445cd40a458de359efff01663d029ed27
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-31 02:51:46 +0000

    ADD:ユーザとメニュを紐づけた

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,6 +3,9 @@ require 'will_paginate/active_record'
 
 class MenusController < ApplicationController
   def index
-    @menus = Menu.all.paginate(page: params[:page], per_page: 10)
+    @menus = nil
+    if logged_in?
+      @menus = current_user.menus.paginate(page: params[:page], per_page: 10)
+    end
   end
 end
