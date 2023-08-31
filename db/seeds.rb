0e3aa99e646a1c5269ce4f870f7d94553c27ecfc
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-31 04:07:18 +0000

    FIX:db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Item.create!( product_name: "コーヒー フラペチーノ short",
-              price: 475,
-              cal: 170,
-              food_or_drink: "drink")
-              
-Item.create!( product_name: "コールドブリュー コーヒー short",
-              price: 400,
-              cal: 9,
-              food_or_drink: "drink")
-              
-Item.create!( product_name: "スターバックス ラテ short",
-              price: 449,
-              cal: 148,
-              food_or_drink: "drink")
 
 require 'csv'
 
