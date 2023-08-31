176d4ceeb5e6255800a21bfbc4afa0b76f130280
88-yana <85784752+88-yana@users.noreply.github.com>
2023-08-31 02:02:10 +0000

    ADD:login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   post '/', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'menus/index'
-
+  
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  get "/logout",  to: "sessions#destroy"
+  get    "/signup",  to: "users#new"
   resources :users
 end
