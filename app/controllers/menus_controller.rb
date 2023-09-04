b03be6c6049b36efbe6e7d64333be97fdca93314
sinagaki <soma0822soma@gmail.com>
2023-09-04 05:10:01 +0000

    Add food+drink

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -24,27 +24,63 @@ class MenusController < ApplicationController
   end
   
   def spin_gacha
-    @menu = spin(params[:max].to_i)
+    @menu = spin(params[:max].to_i, params[:drink])
     if logged_in?
       @menu.users << current_user
     end
-      @menu.save
+    @menu.save
+    redirect_to controller: 'static_pages', action: 'home', id: @menu.id
+  end
+  
+  def spin_gacha_drink_food
+    @menu = spin_drink_food(params[:max].to_i)
+    if logged_in?
+      @menu.users << current_user
+    end
+    @menu.save
     redirect_to controller: 'static_pages', action: 'home', id: @menu.id
   end
 
   private
 
-    def spin(max)
+# drinkは"drink" or nil
+    def spin(max, drink)
       menu = Menu.new
-      valid_items = Item.where("price <= ?", max)
-      while (valid_items.any?)
-        item = valid_items.sample
-          menu.items.push(item)
+      while item = random_item(max, drink)
+        menu.items.push(item)
         max -=  item.price
         menu.price += item.price
         menu.cal += item.cal
-        valid_items = Item.where("price <= ?", max)
       end
       return menu
     end
+    
+    # 一つ目はドリンク２つ目以降はフードのメニューを作る
+    def spin_drink_food(max)
+      menu = Menu.new
+      if item = random_item(max, "drink")
+        menu.items.push(item)
+        max -=  item.price
+        menu.price += item.price
+        menu.cal += item.cal
+      end
+      while item = random_item(max, "food")
+        menu.items.push(item)
+        max -=  item.price
+        menu.price += item.price
+        menu.cal += item.cal
+      end
+      return menu
+    end
+    
+    # フードかドリンクか指定してランダムにitemを一つ取得する
+    def random_item(max, food_or_drink)
+      if food_or_drink
+        valid_items = Item.where("price <= ? AND food_or_drink = ?", max, food_or_drink)
+      else
+        valid_items = Item.where("price <= ?", max)
+      end
+      return valid_items.any? ? valid_items.sample : nil
+    end
+    
 end
