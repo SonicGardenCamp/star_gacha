768ed434054da2fccf44df94086e3af8f8c457f8
88-yana <85784752+88-yana@users.noreply.github.com>
2023-09-06 02:07:05 +0000

    ADD:topping menu relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,13 @@
 class Menu < ApplicationRecord
   has_many :item_menu_relations, dependent: :delete_all
   has_many :items, through: :item_menu_relations
+
+  has_many :topping_menu_relations, dependent: :delete_all
+  has_many :toppings, through: :topping_menu_relations
+  
   has_many :user_menu_relations, dependent: :delete_all
   has_many :users, through: :user_menu_relations, validate: false
+
   validates :items, presence: true
+  validates :toppings, presence: true
 end
