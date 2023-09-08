c12acb12f057510036a6c0d62c5304fb7089d988
88-yana <85784752+88-yana@users.noreply.github.com>
2023-09-08 05:09:12 +0000

    ADD:update

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,34 +7,21 @@ class DocumentsController < ApplicationController
   def create
     upload_file = params[:upload_file]
     category  = upload_file.original_filename.gsub(/s.csv$/, '')
-    destroy_items(category)
-    if category == "topping"
-      CSV.foreach(csv_path, headers: false) do |row|
-        product_name, price = row[0], row[1].to_i
-        Topping.create!(
-          product_name: product_name,
-          price: price,
-        )
-      end
-    elsif category == "food" || category == "drink"
+    if category == "food" || category == "drink"
       CSV.foreach(upload_file.path, headers: false) do |row|
-        product_name, cal, price, topping1, topping2, topping3 = row[0], row[1].to_i, row[2].to_i, row[3], row[4], row[5]
-        Item.create!(
-          product_name: product_name,
-          cal: cal,
-          price: price,
-          food_or_drink: "drink",
-          topping1: topping1,  
-          topping2: topping2,  
-          topping3: topping3,  
-        )
+        product_name, cal, price, topping1, topping2, topping3, csv_id, exist = row[0], row[1].to_i, row[2].to_i, row[3], row[4], row[5], row[6].to_i, row[7]
+        item = Item.find_by(food_or_drink: category, csv_id: csv_id)
+        if exist == "1"
+          exist = true
+        else
+          exist = false
+        end
+        if item
+          item.update(product_name: product_name, cal: cal, price: price, topping1: topping1, topping2: topping2, topping3: topping3, exist: exist)
+        else
+          Item.create(product_name: product_name, cal: cal, price: price, topping1: topping1, topping2: topping2, topping3: topping3, food_or_drink: category, csv_id: csv_id, exist: exist)
+        end
       end
     end
   end
-  
-  private
-    def destroy_items(category)
-      Item.where("food_or_drink == ?", category).destroy_all
-      Topping.destroy_all if category == "topping"
-    end
 end
